254acbf9685dd9a4f85a681061274118ed7954c7
Bisma-Shah <165302776+Bisma-Shah@users.noreply.github.com>
2024-04-22 23:32:25 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Print welcome message
+console.log(chalk.bold.rgb(284, 284,284)(` \n  \t\t <<<==========================>>>`));
+console.log(chalk.bold.rgb(284, 284,284)(`<<<=======>>> ${chalk.bold.hex(`#57D9EE `)(`Welcome to \`Bisma Shah\` - Simple-Calculator App`)}  <<<=========>>>`));
+console.log(chalk.bold.rgb(284, 284,284)(`\t\t <<<==============================>>>\n`));
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exponent","Modulus"],
+  },
+]);
+
+// Conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+ else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+ else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Exponent") {
+  console.log(Math.pow(answer.firstNumber, answer.secondNumber));
+}
+else if (answer.operator === "Modulus") {
+  console.log(answer.firstNumber % answer.secondNumber);
+}
+else {
+    console.log("select an invalid operator");
+}
+
